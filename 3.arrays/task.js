695570f05fe4f5f5fdb81684fa187e1e5eaca24f
zenitfan88 <nikanenkov@rambler.ru>
2023-01-09 20:36:54 +0400

    add arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+  let compare = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+  return compare;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    } 
+    return acc;
+  }, 0);
+  return result;
 }
