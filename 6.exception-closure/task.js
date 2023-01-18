2b595bb592b08c8ec84f03b40ae78ff6e940ada5
zenitfan88 <nikanenkov@rambler.ru>
2023-01-18 19:08:19 +0400

    fixed getTriagle

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -3,13 +3,13 @@
         let parseNumber = Number.parseFloat(number);
         if (isNaN(parseNumber)) {
             throw new Error ("Невалидное значение");
-            }
-            return parseNumber;
-} catch (err) {
-if (err.name !== "Невалидное значение") {
-throw err;
-}
-}
+        }
+        return parseNumber;
+    } catch (err) {
+        if (err.name !== "Невалидное значение") {
+            throw err;
+        }
+    }
 }
 
 
@@ -19,7 +19,7 @@ function validateCount(number) {
         return parseNumber;
     } catch (err) {
         return err;
-        }
+    }
 }
 
 class Triangle {
@@ -27,10 +27,11 @@ class Triangle {
         this.a = a;
         this.b = b;
         this.c = c;
+
         try {
             if (a + b < c || a + c < b || b + c < a) {
                 throw new Error ("Треугольник с такими сторонами не существует");
-                }
+            }
         } catch (err) {
             if (err.name !== "Треугольник с такими сторонами не существует") {
                 throw err;
@@ -41,18 +42,12 @@ class Triangle {
 
     get perimeter() {
         let perimeter = this.a + this.b + this.c;
-        if (perimeter === undefined){
-            perimeter = "Ошибка! Треугольник не существует"
-        }
         return perimeter;
     }
 
     get area() {
-        let p = this.perimeter / 2;
-        let area = Number((Math.sqrt (p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
-        if (area === undefined){
-            area = "Ошибка! Треугольник не существует"
-        }
+        const p = this.perimeter / 2;
+        const area = Number((Math.sqrt (p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
         return area;
     }
 
@@ -60,11 +55,16 @@ class Triangle {
 
 function getTriangle(a, b, c) {
     try {
-        return getPerimeter(getTriangle);
-    } catch (error) {
-        console.log("Ошибка! Треугольник не существует");
+        const triangle = new Triangle(a, b, c);
+        return triangle;
+    } catch (err) {
+        return Object.freeze({
+            area: "Ошибка! Треугольник не существует",
+            perimeter: "Ошибка! Треугольник не существует",
+            })
     }
-} 
+}
+
 
 
     
