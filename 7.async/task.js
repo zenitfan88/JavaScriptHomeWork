1715c766255ae707a1523b4884b91b459b4726d7
zenitfan88 <nikanenkov@rambler.ru>
2023-01-19 07:38:47 +0400

    init synch

---
+++ 7.async/task.js
@@ -0,0 +1,34 @@
+class AlarmClock {
+    constructor () {
+        this.alarmCollection = [];
+        this.intervalId;
+    }
+
+    addClock(params) {
+        
+    }
+
+    removeClock() {
+
+    }
+
+    getCurrentFormattedTime() {
+
+    }
+
+    start() {
+
+    }
+
+    stop() {
+
+    }
+
+    resetAllCalls() {
+
+    }
+
+    clearAlarms() {
+        
+    }
+}
